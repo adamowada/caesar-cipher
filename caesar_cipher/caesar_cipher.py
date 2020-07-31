34f276f47cffd4f91f7fa0cb91597538c2b7373b
Adam Owada <adamowada@gmail.com>
2020-07-30 23:53:30 -0700

    code breaking function almost works

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -48,5 +48,43 @@ def encrypt(plaintext, key):
 def decrypt(ciphertext, key):
     return encrypt(ciphertext, -key)
 
-print(encrypt('adam owada 1235 was here! ! lol', 30))
-print(decrypt('fifr tcfif 1235 cfx mjwj! ! qtq', 30))
+
+def code_breaker(ciphertext, key):
+    guessed_key = 0
+    largest_tracker = 0
+    with open('./wordlist_10000.txt', 'r') as dictionary:
+        for i in range(2):
+            tracker = 0
+            for cipher_word in ciphertext.split():
+                print(cipher_word)
+                print(decrypt(cipher_word, i))
+                for word in dictionary:
+                    if word == f'{decrypt(cipher_word, i)}':
+                        tracker += 1
+            print(f'for key: {i}, the tracker is {tracker}')
+            if tracker > largest_tracker:
+                largest_tracker = tracker
+                guessed_key = i
+    if guessed_key == key:
+        print(f'The computer guessed the key correctly. The key was {key}. The guessed key was {guessed_key}. The ciphertext was {ciphertext}. The plaintext was {decrypt(ciphertext, key)}.')
+        return
+    else:
+        print('The computer guessed the wrong key.')
+        return    
+
+
+def split_check(sentence):
+    for i in sentence.split():
+        print(i)
+    for i in range(26):
+        print(i)
+
+# print(encrypt('adam owada 1235 was here! ! lol', 30))
+# print(decrypt('fifr tcfif 1235 cfx mjwj! ! qtq', 30))
+
+# code_breaker('house')
+
+# split_check('asdfa asdfa ddd asd eee.')
+
+print(encrypt('house boat car dog cat liter', 1))
+code_breaker('ipvtf cpbu dbs eph dbu mjufs', 1)
